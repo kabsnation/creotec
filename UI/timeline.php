f0f90bd553011472c4ee45de91f369115a998197
hannahclairegm <34728983+hannahclairegm@users.noreply.github.com>
2018-01-03 13:51:25 +0800

    timeline

---
+++ UI/timeline.php
@@ -0,0 +1,207 @@
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Announcements</title>
+
+    <link rel="shortcut icon" type="image/x-icon" href="assets/favicon.ico">
+
+    <!-- Global stylesheets -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,300,100,500,700,900" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link href="assets/css/icons/icomoon/styles.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/core.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/components.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/colors.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/material-dashboard.css" rel="stylesheet" type="text/css">
+    <link href="assets/css/heading.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <!-- /global stylesheets -->
+
+    <!-- Core JS files -->
+    <script type="text/javascript" src="assets/js/plugins/loaders/pace.min.js"></script>
+    <script type="text/javascript" src="assets/js/core/libraries/jquery.min.js"></script>
+    <script type="text/javascript" src="assets/js/core/libraries/bootstrap.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/loaders/blockui.min.js"></script>
+    <!-- /core JS files -->
+
+    <!-- Theme JS files -->
+    <script type="text/javascript" src="assets/js/plugins/forms/validation/validate.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/selects/bootstrap_multiselect.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/inputs/touchspin.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/selects/select2.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/styling/switch.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/styling/switchery.min.js"></script>
+    <script type="text/javascript" src="assets/js/plugins/forms/styling/uniform.min.js"></script>
+
+    <script type="text/javascript" src="assets/js/core/app.js"></script>
+    <script type="text/javascript" src="assets/js/pages/form_validation.js"></script>
+
+    
+    <!-- /theme JS files -->
+    <style type="text/css">
+    .panel-heading{
+        font-family: 'Vollkorn SC', serif;
+        font-size: 16px;
+
+    }
+    .separator{
+        width: 120px;
+        height: 2px;
+        background-color: #2980b9;
+    }
+</style>
+</head>
+<body>
+    <!-- Main navbar -->
+    <div class="navbar navbar-default">
+        <div class="navbar-header">
+            <a class="navbar-brand" href="index.php"><img src="assets/images/logo_light.png" alt=""></a>
+
+            <ul class="nav navbar-nav visible-xs-block">
+                <li><a class="sidebar-mobile-main-toggle"><i class="icon-paragraph-justify3"></i></a></li>
+            </ul>
+        </div>
+
+        <div class="navbar-collapse collapse" id="navbar-mobile">
+            <ul class="nav navbar-nav">
+                <li><a class="sidebar-control sidebar-main-toggle hidden-xs"><i class="icon-paragraph-justify3"></i></a></li>
+            </ul>
+
+            <div class="navbar-right">
+                <ul class="nav navbar-nav">
+                    </li>
+
+                    <li class="dropdown dropdown-user">
+                        <a class="dropdown-toggle" data-toggle="dropdown">
+                            <span>Username</span>
+                            <i class="caret"></i>
+                        </a>
+
+                        <ul class="dropdown-menu dropdown-menu-right">
+                            <!-- <li><a href="#"><i class="icon-cog5"></i> Account settings</a></li> -->
+                            <li><a href="#"><i class="icon-switch2"></i> Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+    <!-- /main navbar -->
+
+    <!-- Page container -->
+    <div class="page-container">
+
+        <!-- Page content -->
+        <div class="page-content">
+
+            <!-- Main sidebar -->
+            <div class="sidebar sidebar-main sidebar-default">
+                <div class="sidebar-content">
+
+                    <!-- Main navigation -->
+                    <div class="sidebar-category sidebar-category-visible">
+                        <div class="category-content no-padding">
+                            <ul class="navigation navigation-main navigation-accordion">
+
+                                <!-- Main -->
+                                <li class="navigation-header"><span>Main</span> <i class="icon-menu" title="Main"></i></li>
+                                <li><a href="#"><i class="icon-newspaper"></i> <span>Home</span></a></li>
+                                <li class="navigation-header"><span>Publish</span> <i class="icon-menu" title="Publish"></i></li>
+                                <li class="active"><a href="index.php"><i class="icon-pencil7"></i> <span>Publish News</span></a></li>
+                                
+                            </ul>
+                        </div>
+                    </div>
+                    <!-- /main navigation -->
+
+                </div>
+            </div>
+            <!-- /main sidebar -->
+            <form action="#" class="form-validate-jquery">
+            <div class="content">
+                <div class="container-fluid">
+                    <div class="header text-center">
+                        <!-- <h3 class="title"</h3> -->
+                    </div>
+                    
+                            <!-- <div class="card card-plain">
+                                <div class="card-content"> -->
+                                    <ul class="timeline">
+                                        <!-- <li class="timeline-inverted"> -->
+                                           <!--  <div class="timeline-badge danger">
+                                                <i class="material-icons"></i>
+                                            </div> -->
+                                            
+                                            
+                                            <div class="panel panel-flat">
+                                                <div class="panel-heading" >
+                                                    <center><h1><b>Some Title</b></h1><p style="margin-top: -15px;">By: Juan dela Cruz</p></center>
+                                                    <hr class="separator blue" />
+                                                </div>
+
+
+                                                <div class="panel-body">
+                                                    <p>Wifey made the best Father's Day meal ever. So thankful so happy so blessed. Thank you for making my family We just had fun with the “future” theme !!! It was a fun night all together ... The always rude Kanye Show at 2am Sold Out Famous viewing @ Figueroa and 12th in downtown.</p>
+
+                                                   <!-- <button class="btn btn-info btn-md" style="margin-left: 550px;" role="button"></button> -->
+                                                   <center><a href="#" >See More...</link></a><center>
+                                                </div>
+                                            <div class="panel-footer" style="margin-left: 15px;">
+                                                <i class="fa fa-clock-o" aria-hidden="true" style="margin-left: 790px;"></i> January 4, 2017 | 11 hours ago
+                                            </div>
+                                            </div>
+                                         <div class="panel panel-flat">
+                                                <div class="panel-heading" >
+                                                    <center><h1><b>Some Title</b></h1><p style="margin-top: -15px;">By: Juan dela Cruz</p></center>
+                                                    <hr class="separator blue" />
+                                                </div>
+
+
+                                                <div class="panel-body">
+                                                    <p>Wifey made the best Father's Day meal ever. So thankful so happy so blessed. Thank you for making my family We just had fun with the “future” theme !!! It was a fun night all together ... The always rude Kanye Show at 2am Sold Out Famous viewing @ Figueroa and 12th in downtown.</p>
+
+                                                   <!-- <button class="btn btn-info btn-md" style="margin-left: 550px;" role="button"></button> -->
+                                                   <center><a href="#" >See More...</link></a><center>
+                                                </div>
+                                            <div class="panel-footer" style="margin-left: 15px;">
+                                                <i class="fa fa-clock-o" aria-hidden="true" style="margin-left: 790px;"></i> January 4, 2017 | 11 hours ago
+                                            </div>
+                                            </div>
+
+                                            <div class="panel panel-flat">
+                                                <div class="panel-heading" >
+                                                    <center><h1><b>Some Title</b> </h1><p style="margin-top: -15px;">By: Juan dela Cruz</p></center>
+                                                    <hr class="separator blue" />
+                                                </div>
+
+
+                                                <div class="panel-body">
+                                                    <p>Wifey made the best Father's Day meal ever. So thankful so happy so blessed. Thank you for making my family We just had fun with the “future” theme !!! It was a fun night all together ... The always rude Kanye Show at 2am Sold Out Famous viewing @ Figueroa and 12th in downtown.</p>
+
+                                                   <!-- <button class="btn btn-info btn-md" style="margin-left: 550px;" role="button"></button> -->
+                                                   <center><a href="#" >See More...</link></a><center>
+                                                </div>
+                                            <div class="panel-footer" style="margin-left: 15px;">
+                                                <i class="fa fa-clock-o" aria-hidden="true" style="margin-left: 790px;"></i> January 4, 2017 | 11 hours ago
+                                            </div>
+                                            </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+
+                                
+
+                                </div>
+                            </form>
+                            <!-- /basic layout -->
+
+                       
+</body>
+</html>
